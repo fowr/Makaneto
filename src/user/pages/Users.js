831358ce67148945adf3e27dd6b6bf0ab1626b43
Four <4li.heky@gmail.com>
2021-04-26 20:27:21 +0430

    Users page with some basic styles

---
+++ src/user/pages/Users.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserList from '../components/UserList'
+const Users=()=>{
+const USERS=[{id:'u1',name:'ali Hekmat',places:3,image:'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png'}]
+    return <UserList items={USERS}/>;
+}
+
+export default Users;
