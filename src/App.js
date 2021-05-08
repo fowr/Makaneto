525c7451e8cd4fe8a89ec192758465d36c0ae81b
Four <4li.heky@gmail.com>
2021-05-08 07:17:59 +0430

    Some little changes in UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import './App.css';
+import './App.scss';
 import {BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom'
 import Users from './user/pages/Users';
 import Newplace from './places/pages/NewPlace'
-
+import MainNavigation from './shared/components/Navigation/MainNavigation'
+import UserPlaces from './places/pages/UserPlaces'
 
 const App=()=> {
-  return   <Router>
+  return   (  <Router>
+    <MainNavigation/>
+    <main>
+    <Switch>
+      <Route path="/" exact > 
+        <Users/> 
+      </Route>
+      <Route path="/:userId/places" exact>
+        <UserPlaces/>
+      </Route>
+      <Route path="/places/new" exact>
+        <Newplace/>
+      </Route>
+      <Redirect to="/"/>
+    </Switch>
+    </main>
+   
+    
+  
+</Router>)
 
-  <Switch>
-    <Route path="/" exact > 
-      <Users/> 
-    </Route>
-    <Route path="/places/new" exact>
-      <Newplace/>
-    </Route>
-    <Redirect to="/"/>
-  </Switch>
-  
-  
-</Router>
 
 
 
