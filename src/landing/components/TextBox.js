34c5c57656141a085a0ea275cda82ad6ecb4a238
soheil rahsaz <soheilrahsaz.sr@gmail.com>
2021-05-09 21:45:59 +0430

    Textbox component

---
+++ src/landing/components/TextBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Textbox.scss'
+
+const TextBox = () => {
+    return ( <div className="header__text-box">
+    <h1 className="heading-primary">
+      <span className="heading-primary__main">پرسپکتیو</span>
+      <span className="heading-primary__sub">شبکه <span className='text-bold'>حرفه ای</span> اشتراک گذاری عکس </span>
+    </h1>
+
+    <a href="" class="mybtn mybtn-white">ورود / عضویت</a>
+  </div>
+);
+}
+ 
+export default TextBox;
