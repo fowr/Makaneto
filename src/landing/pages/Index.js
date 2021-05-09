b73d425bba909c7110d42c0ad06e5802a784003f
soheil rahsaz <soheilrahsaz.sr@gmail.com>
2021-05-09 21:47:46 +0430

    Landing page component

---
+++ src/landing/pages/Index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainHeader from '../components/MainHeader'
+import AboutSection from '../components/AboutSection'
+import './index.scss'
+
+const Index= ()=>{
+    return(<React.Fragment>
+        <MainHeader/>
+       <AboutSection/>
+    </React.Fragment>
+      
+    )
+}
+
+export default Index
