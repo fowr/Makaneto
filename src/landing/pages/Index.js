f594d1ac73c78ef0c0d4637a26ab17a6ab5a01fd
soheil rahsaz <soheilrahsaz.sr@gmail.com>
2021-05-09 22:03:11 +0430

    Update Index.js

--- src/landing/pages/Index.js
+++ src/landing/pages/Index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import MainHeader from '../components/MainHeader'
-import AboutSection from '../components/AboutSection'
+
 import './index.scss'
 
 const Index= ()=>{
     return(<React.Fragment>
         <MainHeader/>
-       <AboutSection/>
+
     </React.Fragment>
       
     )
